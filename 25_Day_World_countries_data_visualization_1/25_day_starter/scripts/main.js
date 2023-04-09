53e2af896b1b569cfdc7693893b1ef5413facc12
Adachima <sapondanaisriwan@gmail.com>
2023-04-09 11:09:29 +0700

    .

--- 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
@@ -1,2 +1,58 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const languages = {};
+countries_data.forEach((country) => {
+  country.languages.forEach((language) => {
+    if (language in languages) {
+      languages[language]++;
+    } else {
+      languages[language] = 1;
+    }
+  });
+});
+
+const sortedLanguages = Object.keys(languages)
+  .sort((a, b) => {
+    return languages[b] - languages[a];
+  })
+  .slice(0, 10)
+  .reduce((obj, lang) => {
+    obj[lang] = languages[lang];
+    return obj;
+  }, {});
+
+  const capitalPopulationObj = {};
+
+  countries_data.forEach(country => {
+    const { capital, population } = country;
+    capitalPopulationObj[capital] = population;
+  });
+  
+  const top10CapitalPopulation = Object.entries(capitalPopulationObj)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .reduce((obj, [capital, population]) => {
+      obj[capital] = population;
+      return obj;
+    }, {});
+  
+  console.log(top10CapitalPopulation);
+
+const getStat = document.querySelector("#stat");
+const getLangBtn = document.querySelector(".languages");
+const getSubtitle = document.querySelector("main .subtitle");
+
+getLangBtn.addEventListener("click", () => {
+  getSubtitle.innerHTML = "10 Most Spoken languages in the world";
+  getStat.innerHTML = "";
+  for (const [key, value] of Object.entries(sortedLanguages)) {
+    const textHTML = `
+    <div class="container">
+      <div style="width: 100%;" class="container">
+        <span>${key}</span>
+        <div class="graph-width" style="width: ${value}%"></div>
+      </div>
+      <span>${value}</span>
+    </div>
+    `;
+    getStat.innerHTML += textHTML;
+  }
+});
--- 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
+++ 26_Day_World_countries_data_visualization_2/26_day_starter/scripts/main.js
@@ -1,2 +1,15 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const containerElement = document.querySelector(".container");
+const inputValue = document.querySelector("input").value;
+const startWordBtn = document.querySelector(".starting-word");
+const anyWordBtn = document.querySelector(".any-word");
+const sortBtn = document.querySelector(".sort-word");
+
+
+countries.forEach((country) => {
+  const boxHTML = `
+    <div class="box">
+      <h1>${country}</h1>
+    </div>
+  `;
+  containerElement.innerHTML += boxHTML;
+});
